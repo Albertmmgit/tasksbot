be50442c68fc1378d9936f5f6536a06263168680
Albert Martinez <161922205+Albertmmgit@users.noreply.github.com>
2025-02-09 09:49:08 +0100

    .

--- dist/gpt.js
+++ dist/gpt.js
@@ -33,7 +33,7 @@ const createResponse = async (ctx, date) => {
             {
             "action": "Aquí escribe la acción", 
             "description": "Aquí escribe la tarea", 
-            "pending": "devolver con 'true' si en ${ctx} interpretas que el usuario solo quiere ver tareas pendienes de realizar"
+            "pending": "devolver con 'true' si en ${ctx} interpretas que el usuario solo quiere ver tareas pendienes de realizar. Solo debes añadir el parametro pending en caso que sea true si no no incluyas pending"
             "expirationDate" (si el usuario la indica): "Aquí escribe la fecha con formato YYYY-MM-DD",
             "completed" (if exists): "Aquí escribe 'true'" 
              en caso que la tarea sea completada
